563e09078cf4538cd65e8bffb799e65f46da4cc7
skgydnjs@gmail.com <skgydnjs@gmail.com>
2022-12-25 22:05:17 +0900

    ch12

---
+++ src/jungsuk/exercise/ch12/Ex12_1.java
@@ -0,0 +1,30 @@
+package jungsuk.exercise.ch12;
+
+import java.util.ArrayList;
+
+class Product{}
+class Tv extends Product{}
+class Audio extends Product{}
+
+public class Ex12_1 {
+    public static void main(String[] args) {
+        ArrayList<Product> productList = new ArrayList<>();
+        ArrayList<Tv>           tvList = new ArrayList<>();
+
+        productList.add(new Tv());
+        productList.add(new Tv());
+
+        tvList.add(new Tv());
+        tvList.add(new Tv());
+
+        printAll(productList);
+//        printAll(tvList);  // 컴파일 에러, 서로 상속관계에 있어도 지네릭 타입이 일치해야 한다.
+    }
+
+    public static void printAll(ArrayList<Product> list) {
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+}
---
+++ src/jungsuk/exercise/ch12/Ex12_2.java
@@ -0,0 +1,41 @@
+package jungsuk.exercise.ch12;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Ex12_2 {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student("자바왕", 1, 1));
+        list.add(new Student("자바짱", 1, 2));
+        list.add(new Student("홍길동", 2, 1));
+
+        Iterator<Student> it = list.iterator(); // Iterator에 제네릭스를 적용
+        while(it.hasNext()) {
+            Student s = it.next();
+//            Student s = (Student)it.next(); // 제네릭스를 사용하지 않으면 형변환 필요
+            System.out.println(s.name);
+        }
+
+        Iterator it2 = list.iterator(); // Iterator에 제네릭스를 적용
+        while(it2.hasNext()) {
+//            Student s = it2.next();
+            Student s = (Student)it2.next(); // 제네릭스를 사용하지 않으면 형변환 필요
+            System.out.println(s.name);
+        }
+    }
+}
+
+class Student {
+    String name = "";
+    int ban;
+    int no;
+
+    Student(String name, int ban, int no) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+
+    }
+}
